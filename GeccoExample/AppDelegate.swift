01964d270d191210c13b01d1f3b51033efbd958f
yukiasai <yukiasai@gmail.com>
2016-09-15 18:34:42 +0900

    Resolve the warning of cocoapods.

--- GeccoExample/AppDelegate.swift
+++ GeccoExample/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    fileprivate func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable: Any]?) -> Bool {
         UIApplication.shared.statusBarStyle = .lightContent
         
         return true
