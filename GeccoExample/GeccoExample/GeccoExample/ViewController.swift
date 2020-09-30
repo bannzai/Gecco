1ebc0fc53237ef8c47f858c5be0b9a992e75ba1f
bannzai <kingkong999yhirose@gmail.com>
2020-09-30 15:27:33 +0900

    :sparkels: create new Gecco project

---
+++ GeccoExample/GeccoExample/GeccoExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeccoExample
+//
+//  Created by yudai-hirose on 2020/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
